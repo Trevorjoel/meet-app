78b47ab8446f13df4ce5deb9a1401999cce1fb7f
Trevorjoel <trevsstuff@hotmail.com>
2016-11-12 18:06:42 +0300

    Test git ignore comands

---
+++ connectvars.php
@@ -0,0 +1,7 @@
+<?php
+  // Define database connection constants
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'root');
+  define('DB_PASSWORD', 'JrpUjMRP2I');
+  define('DB_NAME', 'mismatchdb');
+?>

971c908cab1724635eae69893387e46babe5a4b1
Trevorjoel <trevsstuff@hotmail.com>
2016-11-12 18:16:06 +0300

    Delete connectvars.php

--- connectvars.php
+++
@@ -1,7 +0,0 @@
-<?php
-  // Define database connection constants
-  define('DB_HOST', 'localhost');
-  define('DB_USER', 'root');
-  define('DB_PASSWORD', 'JrpUjMRP2I');
-  define('DB_NAME', 'mismatchdb');
-?>
